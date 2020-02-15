f8f5fd7e25ad3dab079dc65bfc496b5d74b1ac93
Farhad Alemi <farhad.alemi@berkeley.edu>
2020-02-14 19:56:38 -0800

    FINISHED 2.2

--- hw2/arrays/Arrays.java
+++ hw2/arrays/Arrays.java
@@ -1,12 +1,9 @@
 package arrays;
 
-/* NOTE: The file Arrays/Utils.java contains some functions that may be useful
- * in testing your answers. */
-
 /** HW #2 */
 
 /** Array utilities.
- *  @author
+ *  @author Farhad Alemi
  */
 class Arrays {
 
@@ -14,16 +11,20 @@ class Arrays {
     /** Returns a new array consisting of the elements of A followed by the
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] result = new int[A.length + B.length];
+        System.arraycopy(A, 0, result, 0, A.length);
+        System.arraycopy(B, 0, result, A.length, B.length);
+        return result;
     }
 
     /* C2. */
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] result = new int[A.length - len];
+        System.arraycopy(A, 0, result, 0, start);
+        System.arraycopy(A, start + len, result, start, A.length - len - start);
+        return result;
     }
 
     /* C3. */
@@ -33,6 +34,8 @@ class Arrays {
      *  returns the three-element array
      *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
     static int[][] naturalRuns(int[] A) {
+        //arraycopy(Object source_arr, int sourcePos, Object dest_arr, int destPos, int len)
+
         /* *Replace this body with the solution. */
         return null;
     }
--- hw2/arrays/ArraysTest.java
+++ hw2/arrays/ArraysTest.java
@@ -3,14 +3,60 @@ package arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** FIXME
- *  @author FIXME
+/**
+ * Tests the Arrays class
+ *
+ *  @author Farhad Alemi
  */
-
 public class ArraysTest {
-    /** FIXME
-     */
 
+    /**
+     * Tests the Arrays.catenate method
+     */
+    @Test
+    public void catenateTest() {
+        int[] test1A = {1, 2, 3, 3};
+        int[] test1B = {3, 2, 3, 3, 3, 1};
+        int[] test1C = {1, 2, 3, 3, 3, 2, 3, 3, 3, 1};
+        assertTrue(Utils.equals(Arrays.catenate(test1A, test1B), test1C));
+
+        int[] test2A = {3};
+        int[] test2B = {};
+        int[] test2C = {3};
+        assertTrue(Utils.equals(Arrays.catenate(test2A, test2B), test2C));
+
+        int[] test3A = {};
+        int[] test3B = {};
+        int[] test3C = {};
+        assertTrue(Utils.equals(Arrays.catenate(test3A, test3B), test3C));
+    }
+
+    /**
+     * Tests the Arrays.remove method
+     */
+    @Test
+    public void removeTest() {
+        int[] test1A = {1, 1, 3, 3};
+        int[] test1B = {1, 1, 3, 3};
+        assertTrue(Utils.equals(Arrays.remove(test1A,0, 0), test1B));
+
+        int[] test2A = {3};
+        int[] test2B = {};
+        assertTrue(Utils.equals(Arrays.remove(test2A, 0, 1), test2B));
+
+        int[] test3A = {};
+        int[] test3B = {};
+        assertTrue(Utils.equals(Arrays.remove(test3A, 0, 0), test3B));
+
+        int[] test4A = {1, 2, 3, 3};
+        int[] test4B = {1, 2};
+        assertTrue(Utils.equals(Arrays.remove(test4A, 2, 2), test4B));
+
+        int[] test5A = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] test5B = {1, 2, 7, 8};
+        assertTrue(Utils.equals(Arrays.remove(test5A, 2, 4), test5B));
+
+    }
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArraysTest.class));
     }
